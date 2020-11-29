198d0441f84425ace035565ec29a2d96297685fa
Kenneth Y <kyang@eastsideprep.org>
2020-11-29 14:49:30 -0800

    Created bindings for python and C. Works in place, but not with Blender

---
+++ Pre-work/PythonAndC/RunMain.sh
@@ -0,0 +1,1 @@
+blender -b -P ./main.py
---
+++ Pre-work/PythonAndC/main.py
@@ -0,0 +1,6 @@
+import sys
+# import bpy
+sys.path.insert(1, "/media/microbobu/WorkDrive/Tech/ERPT-Render-Engine/Pre-work/PythonAndC/cmake-build-debug")
+import PythonAndC as pac
+
+print(pac.add(6, 8))
