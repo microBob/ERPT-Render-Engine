b788f57053c387644815c54c1c035315a583157d
Kenneth Y <kyang@eastsideprep.org>
2020-12-19 11:16:31 -0800

    timed string transfer (working on byte)

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main() {
 
 	char inputBuffer[1024] = {0};
 
+	cout << "Connecting to addon" << endl;
 
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		cout << "Socket creation error" << endl;
@@ -31,16 +32,27 @@ int main() {
 	pixData[0] = 1.83f;
 	pixData[1] = 2.0f;
 
+	cout << "Begin Conversion" << endl;
+
+	auto beforeConvert = high_resolution_clock::now();
+
 	stringstream ss;
 	for (int i = 0; i < pixDataSize / sizeof(float); i++) {
-		ss << pixData[i] << ' ';
+		ss << pixData[i] << ' '; // 2 sec
 	}
+
+	auto afterConvert = high_resolution_clock::now();
+	int convertDur = duration_cast<milliseconds>(afterConvert - beforeConvert).count();
+	cout << "Conversion Duration: " << convertDur << endl;
 
 
 //	send(sock, data.data(), data.size(), 0);
 	send(sock, ss.str().c_str(), ss.str().length(), 0);
 
-	cout << "Message sent!" << endl;
+	auto afterSend = high_resolution_clock::now();
+	int sendDur = duration_cast<milliseconds>(afterSend - afterConvert).count();
+
+	cout << "Message sent! Took: " << sendDur << endl;
 
 	/// Get input
 //	readIn = read(sock, inputBuffer, 1024);
